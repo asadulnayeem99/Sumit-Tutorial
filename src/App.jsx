c039780365901d1053abf5beccc99bdf50ba588a
Md Asadul Islam Nayeem <asadulnayeem99@gmail.comm>
2023-12-03 23:55:23 +0600

    Higher Component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ClickCounter from "./Components/ClickCounter";
+import HoverCounter from "./Components/HoverCounter";
+function App() {
+  return (
+    <>
+      <ClickCounter />
+      <HoverCounter />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/HoverCounter.jsx
@@ -0,0 +1,10 @@
+import withCounter from "./HOC/withCounter";
+const HoverCounter = (props) => {
+  const { count, incrementCount } = props;
+  return (
+    <div>
+      <h1 onMouseOver={incrementCount}>Hovered {count} Times</h1>
+    </div>
+  );
+};
+export default withCounter(HoverCounter);

6843981cb5a6b26706d53e38297fa82c473b7077
Md Asadul Islam Nayeem <asadulnayeem99@gmail.comm>
2023-12-10 22:55:38 +0600

    useMemoHook

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import Button from "./Components/Button";
 import ShowCount from "./Components/ShowCount";
 import Title from "./Components/Title";
@@ -13,11 +13,20 @@ function App() {
     setCount2((prevCount) => prevCount + 5);
     // setCount2(count2+1) dile [count1] evabe bole dite hobe
   }, []);
+  const isEvenOrOdd = useMemo(() => {
+    let i = 0;
+    while (i < 1000000000) {
+      i += 1;
+    }
+    return count1 % 2 === 0;
+  }, [count1]);
   return (
     <>
       <Title />
       <ShowCount count={count1} title="Counter 1" />
+      <span>{isEvenOrOdd ? "Even" : "Odd"}</span>
       <Button handleClick={incrementByOne}>Increment By One</Button>
+
       <hr />
       <ShowCount count={count2} title="Counter 2" />
       <Button handleClick={incrementByFive}>Increment By Five</Button>

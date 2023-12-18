7cf011615dc4856ee262046cce650b9238caa94e
Md Asadul Islam Nayeem <asadulnayeem99@gmail.comm>
2023-12-19 00:30:10 +0600

    styles

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./assets/css/global.css";
+import Logo from "./components/Logo";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Logo />
+    </div>
+  );
+};
+
+export default App;

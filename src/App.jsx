779584fc9ed3933a45ee975a984a13b64e62f06c
Md Asadul Islam Nayeem <asadulnayeem99@gmail.comm>
2023-12-19 18:53:05 +0600

    Global

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import Tutorial from "./Components/Tutorial";
-
+import GlobalStyle from "./Components/styles/Global.styles";
 const App = () => {
   return (
     <>
+      <GlobalStyle />
       <Tutorial />
     </>
   );
